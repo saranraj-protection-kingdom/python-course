967861ad73e05d10f57987ad93480a8dd8ef410c
saranraj-protection-kingdom <35962095+saranraj-protection-kingdom@users.noreply.github.com>
2018-01-30 21:03:32 +0530

    Create greatest_three_num.py

---
+++ greatest_three_num.py
@@ -0,0 +1,9 @@
+num1 = input("Enter number 1")
+num2 = input("Enter number 2")
+num3 = input("Enter number 3")
+if (num2<num1 and num3<num1):
+    print ("number 1 is big")
+if (num3<num2):
+    print("Number 2 is  big")
+else:
+    print("Number 3 is big")
