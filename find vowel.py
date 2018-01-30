8ba60b8ad01695f90622900d0366beeaf48afd4f
saranraj-protection-kingdom <35962095+saranraj-protection-kingdom@users.noreply.github.com>
2018-01-30 20:57:15 +0530

    Create find vowel.py

---
+++ find vowel.py
@@ -0,0 +1,5 @@
+str = raw_input ("Enter the str")
+if (str=="a" or str=="A" or str=="e" or str=="E" or str=="i" or str=="I" or str=="o" or str=="O" or str=="u" or str=="U"):
+    print("vowel")
+else:
+    print("consonant")
