82fcea2077af43cc4164e02df6f2009050ba5791
saranraj-protection-kingdom <35962095+saranraj-protection-kingdom@users.noreply.github.com>
2018-01-30 20:45:51 +0530

    Create numeric.py

---
+++ numeric.py
@@ -0,0 +1,7 @@
+num = input ("Enter the number")
+if(num<0):
+    print ("The given number is negative")
+if(num>0):
+    print ("The given number is possitive")
+if(num==0):
+    print("The given number is equal to zero")
