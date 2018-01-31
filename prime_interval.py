28ff4ed7feb7847faf152684b66e7ff43f51e980
saranraj-protection-kingdom <35962095+saranraj-protection-kingdom@users.noreply.github.com>
2018-01-31 14:01:10 +0530

    Create prime_interval.py

---
+++ prime_interval.py
@@ -0,0 +1,9 @@
+num1=int(input("Enter the range 1"))
+num2=int(input("Enter the range 2"))
+for num in range(num1,num2 + 1):
+   if num > 1:
+       for i in range(2,num):
+           if (num % i) == 0:
+               break
+       else:
+           print(num)
