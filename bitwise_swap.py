e3f86f96e8e506fb923ddf7ef84aef2b8af28f5a
saranraj-protection-kingdom <35962095+saranraj-protection-kingdom@users.noreply.github.com>
2018-02-06 05:27:18 +0530

    Create bitwise_swap.py

---
+++ bitwise_swap.py
@@ -0,0 +1,7 @@
+num1=int(raw_input("Enter first number"))
+num2=int(raw_input("Enter second number"))
+print ("Num1: "+str(num1)+"\n"+"Num2: "+str(num2))
+num1=num1+num2
+num2=num1-num2
+num1=num1-num2
+print("After swap\n"+"Num1: "+str(num1)+"\n"+"Num2: "+str(num2))
